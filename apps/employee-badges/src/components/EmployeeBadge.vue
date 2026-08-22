d862bfab647647ad70268b9821e4f9d2a1cabce8
Two <two@e.com>
2026-08-22 14:44:46 +0800

    fix: adjust badge QR and footer spacing

--- apps/employee-badges/src/components/EmployeeBadge.vue
+++ apps/employee-badges/src/components/EmployeeBadge.vue
@@ -13,13 +13,15 @@ const props = defineProps({
       <h3 :class="{ 'is-long': props.employee.name.length > 8 }">{{ props.employee.name }}</h3>
       <span class="badge-name-divider" aria-hidden="true"></span>
       <p :class="{ 'is-long': props.employee.position.length > 14 }">{{ props.employee.position }}</p>
-      <div class="badge-qr">
-        <img :src="props.employee.qrUrl" :alt="`${props.employee.name}的二维码`" />
+      <div class="badge-code-block">
+        <div class="badge-qr">
+          <img :src="props.employee.qrUrl" :alt="`${props.employee.name}的二维码`" />
+        </div>
+        <div class="badge-footer-copy">
+          <span>联想官方体验店</span>
+          <span>请您添加企业微信</span>
+        </div>
       </div>
-    </div>
-    <div class="badge-footer-copy">
-      <span>联想官方体验店</span>
-      <span>请您添加企业微信</span>
     </div>
   </article>
 </template>
